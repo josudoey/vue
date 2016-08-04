6682bb1d3aaa62587303ec78e5ea21526aaa90b2
joey fang <joey@vm5.com>
2016-08-04 15:24:47 +0800

    [modify] add fullscreen example

---
+++ example/app/fullscreen/index.js
@@ -0,0 +1,42 @@
+//ref https://vuejs.org/examples/modal.html
+var Vue = require("vue");
+var vue = Vue.extend({
+  template: '<div v-show="show"><div v-bind:style="mask"><div v-bind:style="wrapper"><div v-bind:style="css"><div v-bind:style="header"><slot name="header"></slot></div><slot></slot></div></div></div></div>',
+  props: {},
+  data: function() {
+    return {
+      show: false,
+      header: {
+        marginTop: 0,
+        color: "#42b983"
+      },
+      mask: {
+        position: "fixed",
+        zIndex: 9998,
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+        backgroundColor: "rgba(0, 0, 0, .5)",
+        display: "table",
+        transition: "opacity.3s ease"
+      },
+      wrapper: {
+        display: "table-cell",
+        verticalAlign: "middle"
+      },
+      css: {
+        width: "300px",
+        margin: "0px auto",
+        padding: "20px 30px",
+        backgroundColor: "#fff",
+        borderRadius: "2px",
+        boxShadow: "0 2px 8px rgba(0, 0, 0, .33)",
+        transition: "all .3s ease",
+        fontFamily: "Helvetica, Arial, sans-serif"
+      }
+    };
+  }
+});
+module.exports = vue;
+
--- example/app/main.js
+++ example/app/main.js
@@ -1,12 +1,14 @@
 var Vue = require("vue");
+var fullscreen = require("./fullscreen");
 var hello = require("./hello");
 
 var vue = new Vue({
   el: '#main',
   components: {
-    "hello": hello
+    "hello": hello,
+    "screen": fullscreen
   },
-  template: "<div><span>{{msg}}</span> <br /> <hello v-ref:joey>joey</hello> <br /> <hello v-ref:joey2></hello></div>",
+  template: '<span><span>{{msg}}</span> <br /> <hello v-ref:joey>joey</hello> <br /> <hello v-ref:joey2>test</hello><br /><button @click="$refs.screen.show = true">open</button><screen v-ref:screen> this is screen <button @click="$refs.screen.show = false">close</button></screen></span>',
   data: {
     msg: "this is example"
   },
@@ -16,6 +18,8 @@ var vue = new Vue({
     setTimeout(function() {
       self.$refs.joey2.text = "joey2";
     }, 3000);
-  }
+  },
+  methods: {}
 });
+window.vue = vue;
 
