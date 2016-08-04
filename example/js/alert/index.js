6c303822cb5746bcf7f75bcb6b30a08a5797fe91
Joey Fang <josudoey@gmail.com>
2016-08-04 15:33:27 +0800

    [add] alert vue plug.

---
+++ example/js/alert/index.js
@@ -0,0 +1,24 @@
+var Vue = require("vue");
+var Plug = {};
+Plug.install = function (Vue, opts) {
+  opts = opts || {};
+  var id = opts.id || "alertVue";
+  var vue = $("#" + id);
+  if (vue.length === 0) {
+    vue = $('<div style="position: absolute; right: 0; margin-bottom: 0;"></div>');
+    vue.prependTo("body");
+  }
+
+  Vue.prototype.$alert = function (msg, ms) {
+    ms = ms || 3000;
+    var el = $('<div class="alert alert-info" style="display: none; padding: 3px 6px 3px 6px; margin-bottom: 3px;">' + msg + '</div>');
+    vue.append(el);
+    el.slideDown(function () {
+      el.delay(ms).slideUp(function () {
+        el.remove();
+      });
+    });
+  };
+};
+module.exports = Plug;
+
